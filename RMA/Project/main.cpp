9d4e818bafcd055760af029971aea1543a62d508
류민아 <ryuminah0718@gmail.com>
2022-12-26 20:09:57 +0900

    [Search] Binary Search 구현

--- RMA/Project/main.cpp
+++ RMA/Project/main.cpp
@@ -5,6 +5,7 @@
 #include "DFS.h"
 #include "BFS.h"
 #include "Sorting.h"
+#include "Search.h"
 #include "etc.h"
 #include "CodingTest.h"
 
@@ -14,19 +15,7 @@ int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	std::vector<vector<int>> costs;
-	std::vector<int> v1 = {0,1,1};
-	std::vector<int> v2 = { 0,2,2 };
-	std::vector<int> v3 = { 1,2,5 };
-	std::vector<int> v4 = { 1,3,1 };
-	std::vector<int> v5 = { 2,3,8 };
-
-
-	costs.push_back(v1);
-	costs.push_back(v2);
-	costs.push_back(v3);
-	costs.push_back(v4);
-	costs.push_back(v5);
-
-	LinkIsland(4,costs);
+	std::vector<int> data = {0,2,4,6,8,10,12,14,16,18};
+	
+	BinarySearch(data,4);
 }
