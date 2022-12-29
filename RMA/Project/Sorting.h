9fe222700e1b179394667b08ee949202bc70836c
류민아 <ryuminah0718@gmail.com>
2022-12-29 18:06:43 +0900

    [Sorting] 프로그래머스 : 야근 지수

--- RMA/Project/Sorting.h
+++ RMA/Project/Sorting.h
@@ -15,3 +15,6 @@ void CountSort(int* arrData, int arrSize, int minValue, int maxValue);
 
 // 22.12.25 프로그래머스 H-Index
 int HIndex(vector<int> citations);
+
+// 22.12.2 프로그래머스 H-Index
+long long WorkOverTime(int n, vector<int> works);
